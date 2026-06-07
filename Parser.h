a948794db5136d2e2ec7cba9b030ec093c251338
Prithiv T <prithivt.2007@gmail.com>
2026-06-08 01:16:12 +0530

    Update Parser.h

---
+++ Parser.h
@@ -0,0 +1,16 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <vector>
+#include <string>
+
+class Parser {
+
+public:
+
+    static std::vector<std::string>
+    tokenize(const std::string& input);
+
+};
+
+#endif
